38a46982159163858dfb9c8e21467963a5810f2c
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-14 15:56:31 -0400

    created steps

---
+++ src/components/BudgetSteps/BudgetSteps.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Steps } from "antd";
+import {
+    UserOutlined,
+    ToolOutlined,
+    UsergroupAddOutlined,
+    DollarOutlined,
+    BankOutlined,
+} from "@ant-design/icons";
+import styles from "./BudgetSteps.module.scss";
+
+const { Step } = Steps;
+
+export default function BudgetSteps(props) {
+    return (
+        <Steps
+            direction="vertical"
+            current={props.page}
+            onChange={props.onChange}
+            className={styles.stepsParent}
+        >
+            <Step title="Labor" icon={<UserOutlined />} />
+            <Step title="Equiptment and Supplies" icon={<ToolOutlined />} />
+            <Step title="Consultants" icon={<UsergroupAddOutlined />} />
+            <Step title="Non-Personnel Expenses" icon={<DollarOutlined />} />
+            <Step title="Overhead" icon={<BankOutlined />} />
+        </Steps>
+    );
+}
