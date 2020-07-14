2f15aba1bc02e5cc32df1fa9eccc118ac8004161
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-14 15:55:43 -0400

    fomatted view

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import ExportCSV from "../../components/ExportCSV";
+import BudgetSteps from "../../components/BudgetSteps";
+import BudgetPage from "../BudgetPage";
+
+// import styles from "./Main.module.scss";
+import { Layout, Col, Row } from "antd";
+const { Content } = Layout;
+
+export default function Main(props) {
+    return (
+        <Content>
+            <Row>
+                <Col
+                    span={5}
+                    pull={5}
+                    style={{
+                        height: "100vh",
+                        position: "fixed",
+                        left: 0,
+                        backgroundColor: "white",
+                        top: "64px",
+                        zIndex: 2,
+                        minWidth: "17px",
+                    }}
+                >
+                    <BudgetSteps onChange={props.onChange} page={props.step} />
+                </Col>
+                <Col span={19} push={5} style={{ textAlign: "center" }}>
+                    <BudgetPage
+                        data={props.data}
+                        step={props.step}
+                        onChange={props.setData}
+                    />
+                    {props.step + 1 === props.data.length && (
+                        <ExportCSV csvData={props.data} fileName={"Pricing and Budget"} />
+                    )}
+                </Col>
+            </Row>
+        </Content>
+    );
+}
