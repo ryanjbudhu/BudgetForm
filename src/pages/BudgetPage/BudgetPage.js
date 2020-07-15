8bc4a47d35744fe78e3b59cf770dc8286fc11d6c
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-15 10:50:56 -0400

    moving to editable table

--- src/pages/BudgetPage/BudgetPage.js
+++ src/pages/BudgetPage/BudgetPage.js
@@ -5,149 +5,318 @@ import { Table, Input, InputNumber, Popconfirm, Form, Button } from "antd";
 
 const capitalize = (tocap) => tocap.charAt(0).toUpperCase() + tocap.slice(1);
 
-const EditableCell = ({
-    editing,
-    dataIndex,
-    title,
-    inputType,
-    record,
-    index,
-    children,
-    ...restProps
-}) => {
-    const inputNode = inputType === "number" ? <InputNumber /> : <Input />;
+const EditableContext = React.createContext();
+
+const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
     return (
-        <td {...restProps}>
-            {editing ? (
-                <Form.Item
-                    name={dataIndex}
-                    style={{
-                        margin: 0,
-                    }}
-                    rules={[
-                        {
-                            required: true,
-                            message: `Please Input ${title}!`,
-                        },
-                    ]}
-                >
-                    {inputNode}
-                </Form.Item>
-            ) : (
-                children
-            )}
-        </td>
+        <Form form={form} component={false}>
+            <EditableContext.Provider value={form}>
+                <tr {...props} />
+            </EditableContext.Provider>
+        </Form>
     );
 };
 
-export default function BudgetPage(props) {
-    console.log(props.data[props.step]);
+const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+}) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef();
+    const form = useContext(EditableContext);
+    useEffect(() => {
+        if (editing) {
+            inputRef.current.focus();
+        }
+    }, [editing]);
 
-    let newObj = props.data[props.step].items[0];
-    const columnNames = Object.keys(newObj).filter(
-        (key) => key !== "key" && key !== "children"
-    );
-    let columns = columnNames.map((key) => ({
-        title: capitalize(key),
-        dataIndex: key,
-        inputType: key === "name" ? "text" : "number",
-        key: key,
-    }));
-
-    const [form] = Form.useForm();
-    // const [data, setData] = useState(props);
-    const [editingKey, setEditingKey] = useState("");
-
-    const isEditing = (record) => record.key === editingKey;
-
-    let columnFieldsValues = {};
-    columnNames.forEach((name) => (columnFieldsValues[name] = ""));
-
-    const edit = (record) => {
+    const toggleEdit = () => {
+        setEditing(!editing);
         form.setFieldsValue({
-            columnFieldsValues,
-            ...record,
+            [dataIndex]: record[dataIndex],
         });
-        console.log(columnFieldsValues);
-        setEditingKey(record.key);
     };
 
-    const cancel = () => {
-        setEditingKey("");
-    };
-
-    const save = async (key) => {
+    const save = async (e) => {
         try {
-            const row = await form.validateFields();
-            const newData = [...props.data[props.step]];
-            const index = newData.findIndex((item) => key === item.key);
-
-            if (index > -1) {
-                const item = newData[index];
-                newData.splice(index, 1, { ...item, ...row });
-                props.setData(newData);
-                setEditingKey("");
-            } else {
-                newData.push(row);
-                props.setData(newData);
-                setEditingKey("");
-            }
+            const values = await form.validateFields();
+            toggleEdit();
+            handleSave({ ...record, ...values });
         } catch (errInfo) {
-            console.log("Validate Failed:", errInfo);
+            console.log("Save failed:", errInfo);
         }
     };
 
-    columns.push({
-        title: "operation",
-        dataIndex: "operation",
-        render: (_, record) => {
-            const editable = isEditing(record);
-            return editable ? (
-                <span>
-                    <Button
-                        type="primary"
-                        onClick={() => save(record.key)}
-                        style={{
-                            marginRight: 8,
-                        }}
-                    >
-                        Save
-                    </Button>
-                    <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-                        <Button type="link">Cancel</Button>
-                    </Popconfirm>
-                </span>
-            ) : (
-                <Button
-                    type="link"
-                    disabled={editingKey !== ""}
-                    onClick={() => edit(record)}
-                >
-                    Edit
-                </Button>
-            );
-        },
-    });
-    console.log(columns);
-    return (
-        <div className={styles.content}>
-            <h1>{props.data[props.step].label}</h1>
-            <Form form={form} component={false}>
-                <Table
-                    components={{
-                        body: {
-                            cell: EditableCell,
-                        },
-                    }}
-                    bordered
-                    dataSource={props.data[props.step].items}
-                    columns={columns}
-                    summary={(items) => {
-                        let total = 0;
-                        items.forEach((item) => {});
-                    }}
-                ></Table>
-            </Form>
-        </div>
+    let childNode = children;
+
+    if (editable) {
+        childNode = editing ? (
+            <Form.Item
+                style={{
+                    margin: 0,
+                }}
+                name={dataIndex}
+                rules={[
+                    {
+                        required: true,
+                        message: `${title} is required.`,
+                    },
+                ]}
+            >
+                <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+            </Form.Item>
+        ) : (
+            <div
+                className="editable-cell-value-wrap"
+                style={{
+                    paddingRight: 24,
+                }}
+                onClick={toggleEdit}
+            >
+                {children}
+            </div>
+        );
+    }
+
+    return <td {...restProps}>{childNode}</td>;
+};
+
+
+
+export default class BudgetPage extends Component {
+    constructor(props) {
+        super(props);
+let newObj = props.data[props.step].items[0];
+    const columnNames = Object.keys(newObj).filter(
+        (key) => key !== "key" && key !== "children"
     );
+    this.columns = columnNames.map((key) => ({
+        title: capitalize(key),
+        dataIndex: key,
+        inputType: key === "name" ? "text" : "number",
+        editable: true,
+        key: key,
+    }));
+        this.columns.push(
+            {
+                title: "operation",
+                dataIndex: "operation",
+                render: (text, record) =>
+                    this.state.dataSource.length >= 1 ? (
+                        <Popconfirm
+                            title="Sure to delete?"
+                            onConfirm={() => this.handleDelete(record.key)}
+                        >
+                            <a>Delete</a>
+                        </Popconfirm>
+                    ) : null,
+            }
+        );
+        this.state = {
+            dataSource: [
+                {
+                    key: "0",
+                    name: "Edward King 0",
+                    age: "32",
+                    address: "London, Park Lane no. 0",
+                },
+                {
+                    key: "1",
+                    name: "Edward King 1",
+                    age: "32",
+                    address: "London, Park Lane no. 1",
+                },
+            ],
+            count: 2,
+        };
+    }
+    /** TODO
+     *  change this.setState to props.setData
+     */
+
+    handleDelete = (key) => {
+        const dataSource = [...this.state.dataSource];
+        this.setState({
+            dataSource: dataSource.filter((item) => item.key !== key),
+        });
+    };
+
+    handleAdd = () => {
+        const { count, dataSource } = this.state;
+        const newData = {
+            key: count,
+            name: `Edward King ${count}`,
+            quantity: 0,
+            rate: 0,
+        };
+        this.setState({
+            dataSource: [...dataSource, newData],
+            count: count + 1,
+        });
+    };
+
+    handleSave = (row) => {
+        const newData = [...this.state.dataSource];
+        const index = newData.findIndex((item) => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        this.setState({
+            dataSource: newData,
+        });
+    };
+
+    render() {
+        const { dataSource } = this.state;
+        const components = {
+            body: {
+                row: EditableRow,
+                cell: EditableCell,
+            },
+        };
+        const columns = this.columns.map((col) => {
+            if (!col.editable) {
+                return col;
+            }
+
+            return {
+                ...col,
+                onCell: (record) => ({
+                    record,
+                    editable: col.editable,
+                    dataIndex: col.dataIndex,
+                    title: col.title,
+                    handleSave: this.handleSave,
+                }),
+            };
+        });
+        return (
+            <div>
+                <Button
+                    onClick={this.handleAdd}
+                    type="primary"
+                    style={{
+                        marginBottom: 16,
+                    }}
+                >
+                    Add a row
+                </Button>
+                <Table
+                    components={components}
+                    rowClassName={() => "editable-row"}
+                    bordered
+                    dataSource={dataSource}
+                    columns={columns}
+                />
+            </div>
+        );
+    }
 }
+
+// export default function BudgetPage(props) {
+//     console.log(props.data[props.step]);
+
+    
+    
+
+//     const [form] = Form.useForm();
+//     // const [data, setData] = useState(props);
+//     const [editingKey, setEditingKey] = useState("");
+
+//     const isEditing = (record) => record.key === editingKey;
+
+//     let columnFieldsValues = {};
+//     columnNames.forEach((name) => (columnFieldsValues[name] = ""));
+
+//     const edit = (record) => {
+//         form.setFieldsValue({
+//             columnFieldsValues,
+//             ...record,
+//         });
+//         setEditingKey(record.key);
+//     };
+
+//     const cancel = () => {
+//         setEditingKey("");
+//     };
+
+//     const save = async (key) => {
+//         try {
+//             const row = await form.validateFields();
+//             const newData = [...props.data[props.step]];
+//             const index = newData.findIndex((item) => key === item.key);
+
+//             if (index > -1) {
+//                 const item = newData[index];
+//                 newData.splice(index, 1, { ...item, ...row });
+//                 props.setData(newData);
+//                 setEditingKey("");
+//             } else {
+//                 newData.push(row);
+//                 props.setData(newData);
+//                 setEditingKey("");
+//             }
+//         } catch (errInfo) {
+//             console.log("Validate Failed:", errInfo);
+//         }
+//     };
+
+//     columns.push({
+//         title: "operation",
+//         dataIndex: "operation",
+//         render: (_, record) => {
+//             const editable = isEditing(record);
+//             return editable ? (
+//                 <span>
+//                     <Button
+//                         type="primary"
+//                         onClick={() => save(record.key)}
+//                         style={{
+//                             marginRight: 8,
+//                         }}
+//                     >
+//                         Save
+//                     </Button>
+//                     <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+//                         <Button type="link">Cancel</Button>
+//                     </Popconfirm>
+//                 </span>
+//             ) : (
+//                 <Button
+//                     type="link"
+//                     disabled={editingKey !== ""}
+//                     onClick={() => edit(record)}
+//                 >
+//                     Edit
+//                 </Button>
+//             );
+//         },
+//     });
+//     console.log(columns);
+//     return (
+//         <div className={styles.content}>
+//             <h1>{props.data[props.step].label}</h1>
+//             <Form form={form} component={false}>
+//                 <Table
+//                     components={{
+//                         body: {
+//                             cell: EditableCell,
+//                         },
+//                     }}
+//                     bordered
+//                     dataSource={props.data[props.step].items}
+//                     columns={columns}
+//                     summary={(items) => {
+//                         let total = 0;
+//                         items.forEach((item) => {});
+//                     }}
+//                 ></Table>
+//             </Form>
+//         </div>
+//     );
+// }
