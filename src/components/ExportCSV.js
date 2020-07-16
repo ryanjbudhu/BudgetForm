82a571a6ab36355a31a5105d8ca89d1733b13a16
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-16 15:50:34 -0400

    created separate sheets for detailed info

--- src/components/ExportCSV.js
+++ src/components/ExportCSV.js
@@ -8,53 +8,89 @@ const ExportCSV = ({ csvData, fileName }) => {
         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
     const fileExtension = ".xlsx";
 
+    // TODO: format the excel to look nice
+
     const exportToCSV = (csvData, fileName) => {
-        const convertedData = dataToExport(csvData);
+        const convertedData = getOverviewSheet(csvData);
         const ws = XLSX.utils.json_to_sheet(convertedData);
-        const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+        let wb = XLSX.utils.book_new();
+        // const overviewBook = { Sheets: { data: ws }, SheetNames: ["Overview"] };
+        XLSX.utils.book_append_sheet(wb, ws, "Overview");
+
+        const detailedSheets = getEachTypeSheet(csvData);
+        detailedSheets.forEach(({ sheetName, sheetData }) => {
+            const pageSheet = XLSX.utils.json_to_sheet(sheetData);
+            XLSX.utils.book_append_sheet(wb, pageSheet, sheetName);
+        });
+
         const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
         const data = new Blob([excelBuffer], { type: fileType });
         FileSaver.saveAs(data, fileName + fileExtension);
     };
 
-    const dataToExport = (data) => {
+    const getEachTypeSheet = (data) => {
+        let sheets = [];
+        data.forEach((pageObj) => {
+            let [sheetTotal, sheetData] = getPageData(pageObj, true);
+            sheetData.push({ pagename: `Total ${pageObj.label}`, total: sheetTotal });
+            sheets.push({
+                sheetName: pageObj.label,
+                sheetData: sheetData,
+            });
+        });
+
+        return sheets;
+    };
+
+    const getPageData = (pageObj, getChildren) => {
+        let htotal = 0;
+        let headers = [];
+        pageObj.items.forEach((header) => {
+            let headerObj;
+            if (getChildren) headerObj = { pagename: `Total ${header.name}` };
+            else headerObj = { pagename: header.name };
+            let childObj;
+            headerObj.quantity = 0;
+            headerObj.rate = 0;
+            headerObj.total = 0;
+            header.children.forEach((child) => {
+                childObj = {
+                    pagename: child.name,
+                    quantity: child.quantity,
+                    rate: child.rate,
+                    total:
+                        Number.parseInt(child.quantity) * Number.parseFloat(child.rate),
+                };
+                headerObj.quantity += Number.parseInt(child.quantity);
+                headerObj.rate += Number.parseFloat(child.rate);
+                headerObj.total += childObj.total;
+            });
+
+            if (header.children.length > 0) {
+                headerObj.rate = headerObj.rate / header.children.length;
+            }
+            htotal += headerObj.total;
+            if (getChildren && childObj !== undefined) {
+                headers.push(childObj);
+            }
+            headers.push(headerObj);
+        });
+        return [htotal, headers];
+    };
+
+    const getOverviewSheet = (data) => {
         let arrData = [[]];
         let total = 0;
         data.forEach((pageObj) => {
             arrData.push({ pagename: pageObj.label });
-            let htotal = 0;
-            pageObj.items.forEach((header) => {
-                let headerObj = { pagename: header.name };
-                let childObj;
-                headerObj.quantity = 0;
-                headerObj.rate = 0;
-                headerObj.total = 0;
-                header.children.forEach((child) => {
-                    childObj = {
-                        pagename: child.name,
-                        quantity: child.quantity,
-                        rate: child.rate,
-                        total:
-                            Number.parseInt(child.quantity) *
-                            Number.parseFloat(child.rate),
-                    };
-                    headerObj.quantity += Number.parseInt(child.quantity);
-                    headerObj.rate += Number.parseFloat(child.rate);
-                    headerObj.total += childObj.total;
-                });
-
-                if (header.children.length > 0) {
-                    headerObj.rate = headerObj.rate / header.children.length;
-                }
-                htotal += headerObj.total;
-                arrData.push(headerObj);
-            });
+            const [pageTotal, pageData] = getPageData(pageObj, false);
+            arrData.push([...pageData]);
             arrData.push({
                 pagename: `Total ${pageObj.label}`,
-                total: htotal.toFixed(2),
+                total: pageTotal.toFixed(2),
             });
             arrData.push([]);
-            total += htotal;
+            total += pageTotal;
         });
         arrData.push({ pagename: "Total", total: total.toFixed(2) });
         return arrData;
