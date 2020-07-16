63aa3cb563cb129e71cd9b347535cfd8be3e596c
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-16 14:49:55 -0400

    converted json data to formatted csv

--- src/components/ExportCSV.js
+++ src/components/ExportCSV.js
@@ -9,12 +9,56 @@ const ExportCSV = ({ csvData, fileName }) => {
     const fileExtension = ".xlsx";
 
     const exportToCSV = (csvData, fileName) => {
-        console.log([...csvData]);
-        const ws = XLSX.utils.json_to_sheet(csvData);
+        const convertedData = dataToExport(csvData);
+        const ws = XLSX.utils.json_to_sheet(convertedData);
         const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
         const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
         const data = new Blob([excelBuffer], { type: fileType });
-        // FileSaver.saveAs(data, fileName + fileExtension);
+        FileSaver.saveAs(data, fileName + fileExtension);
+    };
+
+    const dataToExport = (data) => {
+        let arrData = [[]];
+        let total = 0;
+        data.forEach((pageObj) => {
+            arrData.push({ pagename: pageObj.label });
+            let htotal = 0;
+            pageObj.items.forEach((header) => {
+                let headerObj = { pagename: header.name };
+                let childObj;
+                headerObj.quantity = 0;
+                headerObj.rate = 0;
+                headerObj.total = 0;
+                header.children.forEach((child) => {
+                    childObj = {
+                        pagename: child.name,
+                        quantity: child.quantity,
+                        rate: child.rate,
+                        total:
+                            Number.parseInt(child.quantity) *
+                            Number.parseFloat(child.rate),
+                    };
+                    headerObj.quantity += Number.parseInt(child.quantity);
+                    headerObj.rate += Number.parseFloat(child.rate);
+                    headerObj.total += childObj.total;
+                });
+
+                if (header.children.length > 0) {
+                    headerObj.rate = headerObj.rate / header.children.length;
+                }
+                htotal += headerObj.total;
+                arrData.push(headerObj);
+                // if (childObj !== undefined) arrData.push(childObj);
+            });
+            arrData.push({
+                pagename: `Total ${pageObj.label}`,
+                total: htotal.toFixed(2),
+            });
+            arrData.push([]);
+            total += htotal;
+        });
+        arrData.push({ pagename: "Total", total: total.toFixed(2) });
+        return arrData;
     };
 
     return (
