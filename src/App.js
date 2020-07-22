53fe526ca383c9d29eba0a311776add3d70a4f1c
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-22 10:50:18 -0400

    tried implementing useReducer

--- src/App.js
+++ src/App.js
@@ -18,16 +18,34 @@ const useSessionState = (storageKey) => {
     return [data, setData];
 };
 
+function init() {
+    return defaultData;
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "page": // action = {step, pageData}
+            const newData = [...state.pages];
+            newData[step].items = action.pageData;
+            return { ...state, pages: newData };
+        case "info": // action = {dataItem, infoData}
+            return { ...state, info: { ...state.info, [action.dataItem]: infoData } };
+        default:
+            throw new Error("Action of unknown type.");
+    }
+}
+
 // TODO: change to useReducer and create reducer function
 function App() {
     const [step, setStep] = useState(0);
     const onChange = (current) => setStep(current);
-    const [data, setData] = useSessionState("VLProjectBudget");
-    const setPageData = (pageData) => {
-        const newData = [...data];
-        newData[step].items = pageData;
-        setData(newData);
-    };
+    // const [data, setData] = useSessionState("VLProjectBudget");
+    // const setPageData = (pageData) => {
+    //     const newData = [...data];
+    //     newData[step].items = pageData;
+    //     setData(newData);
+    // };
+    const [state, dispatch] = useReducer(reducer, init);
     return (
         <Layout>
             <Header
