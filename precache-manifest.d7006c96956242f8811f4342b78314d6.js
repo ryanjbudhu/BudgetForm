89bf6e237b62013f4ef6feae9f2b1a7c6bf69751
Ryan Budhu <ryan.j.budhu@gmail.com>
2020-07-29 17:26:51 -0400

    Updated demo site

---
+++ precache-manifest.d7006c96956242f8811f4342b78314d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213bed362b63adec305fe0a0f3345065",
+    "url": "./index.html"
+  },
+  {
+    "revision": "722885f4d5d3aaf2f8c8",
+    "url": "./static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "76d61842156ad337fda9",
+    "url": "./static/css/main.d8033171.chunk.css"
+  },
+  {
+    "revision": "722885f4d5d3aaf2f8c8",
+    "url": "./static/js/2.3825e930.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "./static/js/2.3825e930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d61842156ad337fda9",
+    "url": "./static/js/main.d15d1be7.chunk.js"
+  },
+  {
+    "revision": "7cf455e298bdacc8239d",
+    "url": "./static/js/runtime-main.3ee14ed2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.50481b683a51b97f4f676aee958360bd.js"
+  "./precache-manifest.d7006c96956242f8811f4342b78314d6.js"
 );
 
 self.addEventListener('message', (event) => {
